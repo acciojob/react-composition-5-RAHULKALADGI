2d9c0c92903ebf3a85028ac1277cc8f47933490a
Rahul R Kaladgi <127622361+RAHULKALADGI@users.noreply.github.com>
2023-06-30 18:19:07 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,36 @@
 
-import React from "react";
+import React ,{useState} from "react";
 import './../styles/App.css';
 
-const App = () => {
-  return (
+let a = [{title : "Tab" , content : 1} , {title : "Tab" , content : 2} , {title : "Tab" , content : 3} ]
+
+let b = [{title : "Tab" , content : "A"} , {title : "Tab" , content : "B"} , {title : "Tab" , content : "C"} ]
+
+const App = ()=> {
+   let [count , setCount] = useState(1);
+   let [alpha , setAlpha] = useState("A");
+
+   return (
     <div>
-        {/* Do not remove the main div */}
+        <ul>
+            {
+                a.map((e)=> (
+                    <li onClick={()=>setCount(e.content)}>{e.title} {e.content}</li>
+                ))
+            }
+        </ul>
+        <p>Content for Tab {count}</p>
+
+        <ul>
+            {
+                b.map((e)=> (
+                    <li onClick={()=>setAlpha(e.content)}>{e.title} {e.content}</li>
+                ))
+            }
+        </ul>
+        <p>Content for Tab {alpha}</p>
     </div>
-  )
+   )
 }
 
 export default App
